dc674f44eede9830e78ef9e9e9ba68b690bc4608
usman <usman@cognitivehealthintl.com>
2020-04-30 19:54:46 +0500

    ..

--- 003_Dynamic_Linking/build_standalone.sh
+++ 003_Dynamic_Linking/build_standalone.sh
@@ -1,5 +1,5 @@
 rm -r bin
 mkdir bin
-emcc main.c foo1.c foo2.c -o bin/main.wasm
-#emcc main.c foo1.c foo2.c -s STANDALONE_WASM -o bin/main.wasm
+#emcc main.c foo1.c foo2.c -o bin/main.wasm
+emcc main.c foo1.c foo2.c -s STANDALONE_WASM -o bin/main.wasm
 wasmer run bin/main.wasm
